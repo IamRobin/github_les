2678509a045fc4212c1f6f2a96fbcc0e44818d02
IamRobin <robinjanssen0@gmail.com>
2018-11-21 20:01:39 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const curry = fn => (...args) => fn.bind(null, ...args);
+
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+
+const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
+
+Array.prototype.unwind = function (key) {
+  return this.reduce((acc, curr) =>
+    [].concat(acc, curr[key].map(val => ({ ...curr, [key]: val }))), []);
+};
+
+const arr = [1,23,4,5];
+
+console.log('hello', arr);
